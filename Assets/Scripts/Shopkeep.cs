6122dfde810b062618c326067ecc54066fe19ffe
Adam Gressen <adamgressen@gmail.com>
2014-11-14 21:04:44 -0500

    adding purchasables

--- Assets/Scripts/Shopkeep.cs
+++ Assets/Scripts/Shopkeep.cs
@@ -8,11 +8,23 @@ public class Shopkeep : MonoBehaviour {
 	CharacterController controller;
 	
 	public GameObject mapObj;
+	public GameObject bootsObj;
+	public GameObject swordObj;
+	public GameObject keyObj;
+	public GameObject betterBootsObj;
 	
 	void Start() {
 		playerInRange = false;
 		numPurchased = 0;
+		// maybe make the coin counter free
 		itemsForSale.Add(new Purchasable("map", 10, mapObj));
+		itemsForSale.Add(new Purchasable("boots", 50, bootsObj));
+		itemsForSale.Add(new Purchasable("sword", 100, swordObj));
+		//itemsForSale.Add(new Purchasable("skeleton key", 200, keyObj));
+		//itemsForSale.Add(new Purchasable("better boots", 300, betterBootsObj));
+		//itemsForSale.Add(new Purchasable("", 400, ));
+		// fast travel
+		//itemsForSale.Add(new Purchasable("", 500, ));
 		
 		controller = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<CharacterController>();
 	}
